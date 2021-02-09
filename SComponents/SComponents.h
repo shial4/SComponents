03b844c8e10639714ba2f56549682b88a47d0687
Szymon Lorenz <szymon.lorenz.solutions@gmail.com>
2021-02-10 08:46:22 +1000

    Initial Commit

---
+++ SComponents/SComponents.h
@@ -0,0 +1,18 @@
+//
+//  SComponents.h
+//  SComponents
+//
+//  Created by Szymon Lorenz on 10/2/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SComponents.
+FOUNDATION_EXPORT double SComponentsVersionNumber;
+
+//! Project version string for SComponents.
+FOUNDATION_EXPORT const unsigned char SComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SComponents/PublicHeader.h>
+
+
